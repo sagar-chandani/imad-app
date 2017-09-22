e9712282fee695a641af6d9598361755b78afd48
Sagar Chandani <spc1194@gmail.com>
2017-09-23 05:01:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,24 +4,42 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-
-var articleOne = {
-    title : '   Article One | Sagar Chandani',
-    heading: ' Article One',
-    date : 'Sept 23, 2017',
-    content : `<p>
-                This is the content for my first article. This is the content for my first article. This is the content for my first article
-    		This is the content for my first article.This is the content for my first article.
-            </p>
-            <p>
-                This is the content for my first article. This is the content for my first article. This is the content for my first article
-    		This is the content for my first article.This is the content for my first article.
-            </p>
-            <p>
-                This is the content for my first article. This is the content for my first article. This is the content for my first article
-    		This is the content for my first article.This is the content for my first article.
-            </p> `
-}
+var articles = {
+     articleOne : {
+        title : ' Article One | Sagar Chandani',
+        heading: ' Article One',
+        date : 'Sept 23, 2017',
+        content : `<p>
+                    This is the content for my first article. This is the content for my first article. This is the content for my first article
+        		This is the content for my first article.This is the content for my first article.
+                </p>
+                <p>
+                    This is the content for my first article. This is the content for my first article. This is the content for my first article
+        		This is the content for my first article.This is the content for my first article.
+                </p>
+                <p>
+                    This is the content for my first article. This is the content for my first article. This is the content for my first article
+        		This is the content for my first article.This is the content for my first article.
+                </p> `
+    },
+    articleTwo : {
+        
+        title : ' Article Two | Sagar Chandani',
+        heading: ' Article Two',
+        date : 'Sept 24, 2017',
+        content : `<p>
+                    This is the content for my Second article.`
+    },
+    
+     articleThree : {
+         
+        title : ' Article Three | Sagar Chandani',
+        heading: ' Article Three',
+        date : 'Sept 23, 2017',
+        content : `<p>
+                    This is the content for my third  article.`
+     }
+};
 
 function createTemplate (data) {
         var title = data.title;
