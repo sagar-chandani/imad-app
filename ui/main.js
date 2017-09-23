6a9f58e17d6ba783e70d928d6ec6af6a265b738d
Sagar Chandani <spc1194@gmail.com>
2017-09-23 07:26:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -62,16 +62,35 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
   //make a request to the serveer and send the name
+      var request = new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState ===  XMLHttpRequest.DONE){
+            //take some action
+            
+            if(request.status === 200){
+                  //make the list of names and render is as a list
+                  var names =request.responseText;
+                  names = JSON.parse(names);
+                  var list = '';
+                  for(var i = 0; i<names.length; i++){
+                      list += '<li>' + names[i] + '</li>';
+                  }
+                  
+                  var ul = document.getElementById('namelist');
+                  ul.innerHTML = list;
+                  
+              
+            }
+        }
+        //if not
+    };
+  
+  //Make the request
+  request.open('GET', 'http://spc1194.imad.hasura-app.io/submit-name?name=' +name ,true);
+  request.send(null);
   
   
-  //make the list of names and render is as a list
-  var names = ['name1','name2','name3','name4'];
-  var list = '';
-  for(var i = 0; i<names.length; i++){
-      list += '<li>' + names[i] + '</li>';
-  }
-  
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
-  
+
 };
