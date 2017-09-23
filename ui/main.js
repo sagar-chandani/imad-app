cdfa9278099168077a2415bdd3680670950ac00c
Sagar Chandani <spc1194@gmail.com>
2017-09-23 06:31:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,6 @@
-console.log('Loaded!');
 
+/*
+console.log('Loaded!');
 
 
 //change text to new value
@@ -22,3 +23,18 @@ function moveRight(){
 img.onclick = function(){
   var interval = setInterval(moveRight, 50);
 };
+*/
+
+//Counter code
+
+var button = document.getElementById('counter');
+button.onclick = function(){
+    //Make a request to counter endpoint 
+    
+    //capture the response and store it in a variable
+    
+    //render the value in the span
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+}
