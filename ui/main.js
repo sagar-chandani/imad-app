5d8a7b6fd5394652c407f1c9e28e88cb33c1175d
Sagar Chandani <spc1194@gmail.com>
2017-09-23 05:55:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,16 @@
 console.log('Loaded!');
+
+
+
 //change text to new value
 
 
 var element = document.getElementById('main-text');
 element.innerHTML = "New Value"
+
+//move image to right
+
+var img = document.getElementById('madi');
+img.onClick = function(){
+  madi.style.marginLeft= "100px";  
+};
