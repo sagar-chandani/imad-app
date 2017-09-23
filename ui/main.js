a230a69f779ff54c36eeb038fe37985502828281
Sagar Chandani <spc1194@gmail.com>
2017-09-23 06:04:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,8 +16,9 @@ var  marginLeft = 0;
 function moveRight(){
     
     marginLeft = marginLeft + 10;
-    img.style.marginLeft = marginLeft + "px";
+    img.style.marginLeft = marginLeft + 'px';
 }
+
 img.onclick = function(){
   var interval = setinterval(moveRight, 100);
 };
